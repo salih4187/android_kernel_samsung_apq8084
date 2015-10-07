8d5198ca4f68475d4b9a9ed5e649e1ebbd4749db
Daniel Borkmann <daniel@iogearbox.net>
2015-10-08 01:20:37 +0200

    random32: add prandom_seed_full_state helper
    Factor out the full reseed handling code that populates the state
    through get_random_bytes() and runs prandom_warmup(). The resulting
    prandom_seed_full_state() will be used later on in more than the
    current __prandom_reseed() user. Fix also two minor whitespace
    issues along the way.
    
    Change-Id: I69998a09f1c9bdb9eed3f189bd2569062cc48bfa
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- lib/random32.c
+++ lib/random32.c
@@ -157,7 +157,7 @@ static int __init prandom_init(void)
 	int i;
 
 	for_each_possible_cpu(i) {
-		struct rnd_state *state = &per_cpu(net_rand_state,i);
+		struct rnd_state *state = &per_cpu(net_rand_state, i);
 
 #define LCG(x)	((x) * 69069)	/* super-duper LCG */
 		state->s1 = __seed(LCG(i + jiffies), 2);
@@ -197,22 +197,9 @@ static void prandom_start_seed_timer(void)
 	add_timer(&seed_timer);
 }
 
-/*
- *	Generate better values after random number generator
- *	is fully initialized.
- */
-static void __prandom_reseed(bool late)
+void prandom_seed_full_state(struct rnd_state __percpu *pcpu_state)
 {
 	int i;
-	unsigned long flags;
-	static bool latch = false;
-	static DEFINE_SPINLOCK(lock);
-
-	/* only allow initial seeding (late == false) once */
-	spin_lock_irqsave(&lock, flags);
-	if (latch && !late)
-		goto out;
-	latch = true;
 
 	for_each_possible_cpu(i) {
 		struct rnd_state *state = &per_cpu(net_rand_state,i);
@@ -226,6 +213,23 @@ static void __prandom_reseed(bool late)
 		/* mix it in */
 		prandom_u32_state(state);
 	}
+}
+
+/*
+ *	Generate better values after random number generator
+ *	is fully initialized.
+ */
+static void __prandom_reseed(bool late)
+{
+	unsigned long flags;
+	static bool latch = false;
+	static DEFINE_SPINLOCK(lock);
+ 	/* only allow initial seeding (late == false) once */
+	spin_lock_irqsave(&lock, flags);
+	if (latch && !late)
+		goto out;
+	latch = true;
+
 out:
 	spin_unlock_irqrestore(&lock, flags);
 }
